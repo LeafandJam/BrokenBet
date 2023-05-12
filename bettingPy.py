73716e96ec80342efacb29be48c92efe4e10c240
Leaf <64458399+LeafandJam@users.noreply.github.com>
2023-05-12 14:36:02 +0930

    Update bettingPy.py

--- bettingPy.py
+++ bettingPy.py
@@ -31,12 +31,19 @@ while game == True:
     print('\n\nYou must put in $',investment,'.')
     print('\nThe profit is:\n $', profit1,'\n $',profit2)
 
-    calc2 = input("\n\n Would you like to calculate another game? ('c' will clear the bonus money) [y/n/c] ")
-    if calc2 != 'y' and calc2 != 'c':
+    calc2 = input("\n\n Would you like to calculate another game? \n ('c' will clear the bonus money) ('b' will let you type the name of the team to remember it)\n [y/n/c/b] ")
+    if calc2 != 'y' and calc2 != 'c' and calc2 != 'b':
         print("Good luck!")
         game = False
+
     elif calc2 == 'c':
         bonusMoney = int(input('What is the new bonus money? $'))
+    
+    elif calc2 == 'b':
+        bookmark = input("What team is this? ")
+        print("\n\n The team is: ",bookmark)
+        print('You must put in $',investment,'.')
+        print('The profit is:\n $', profit1,'\n $',profit2,'\n\n')
 
 
 
